4259d425b2e4827369bee00da8e455dadda2da3d
Semyon <semyontikhonenko@gmail.com>
2015-05-22 23:43:59 +0300

    camera gallery buttons test

--- src/com/badmen/EasyFilter/MainActivity.java
+++ src/com/badmen/EasyFilter/MainActivity.java
@@ -1,15 +1,41 @@
 package com.badmen.EasyFilter;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import com.utilsframework.android.image.ImagePreviewActivity;
+import com.utilsframework.android.image.ImageUtils;
 
 public class MainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+    static final int GALLERY = 1;
+    static final int CAMERA = 2;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        findViewById(R.id.gallery).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageUtils.pickImageFromGallery(MainActivity.this, GALLERY);
+            }
+        });
+
+        findViewById(R.id.camera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageUtils.takeImageFromCamera(MainActivity.this, CAMERA);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        String selectedImagePath = ImageUtils.getSelectedImagePath(this, data);
+        ImagePreviewActivity.show(this, BitmapFactory.decodeFile(selectedImagePath));
     }
 }
