204c390ca82e69bcb74d70f7b3a3e4bb9014427f
Semyon <semyontikhonenko@gmail.com>
2015-06-02 12:53:07 +0300

    filters replacement

--- src/com/badmen/EasyFilter/GPUImageFilterTools.java
+++ src/com/badmen/EasyFilter/GPUImageFilterTools.java
@@ -59,7 +59,7 @@ public class GPUImageFilterTools {
                 GPUImageLookupFilter lookupFilter = new GPUImageLookupFilter();
                 Bitmap bitmap = BitmapFactory.decodeStream(assets.open("instagram/" + filterName));
                 lookupFilter.setBitmap(bitmap);
-                filterName = Strings.capitalizeAndCopy(FileUtils.removeExtension(filterName));
+                filterName = getFilterNameFromFileName(filterName);
                 filters.addFilter(filterName, lookupFilter);
             }
         } catch (IOException e) {
@@ -129,177 +129,15 @@ public class GPUImageFilterTools {
         return filters.filters;
     }
 
-//    public static GPUImageFilter createFilterForType(final Context context, final FilterType type) {
-//        switch (type) {
-//            case GAMMA:
-//                return new GPUImageGammaFilter(2.0f);
-//            case INVERT:
-//                return new GPUImageColorInvertFilter();
-//            case PIXELATION:
-//                return new GPUImagePixelationFilter();
-//            case HUE:
-//                return new GPUImageHueFilter(90.0f);
-//            case BRIGHTNESS:
-//                return new GPUImageBrightnessFilter(0.4f);
-//            case GRAYSCALE:
-//                return new GPUImageGrayscaleFilter();
-//            case SEPIA:
-//                return new GPUImageSepiaFilter();
-//            case SHARPEN:
-//                GPUImageSharpenFilter sharpness = new GPUImageSharpenFilter();
-//                sharpness.setSharpness(2.0f);
-//                return sharpness;
-//            case SOBEL_EDGE_DETECTION:
-//                return new GPUImageSobelEdgeDetection();
-//            case THREE_X_THREE_CONVOLUTION:
-//                GPUImage3x3ConvolutionFilter convolution = new GPUImage3x3ConvolutionFilter();
-//                convolution.setConvolutionKernel(new float[]{
-//                        -1.0f, 0.0f, 1.0f,
-//                        -2.0f, 0.0f, 2.0f,
-//                        -1.0f, 0.0f, 1.0f
-//                });
-//                return convolution;
-//            case EMBOSS:
-//                return new GPUImageEmbossFilter();
-//            case POSTERIZE:
-//                return new GPUImagePosterizeFilter();
-//            case FILTER_GROUP:
-//                List<GPUImageFilter> filters = new LinkedList<GPUImageFilter>();
-//                filters.add(new GPUImageContrastFilter());
-//                filters.add(new GPUImageDirectionalSobelEdgeDetectionFilter());
-//                filters.add(new GPUImageGrayscaleFilter());
-//                return new GPUImageFilterGroup(filters);
-//            case SATURATION:
-//                return new GPUImageSaturationFilter(1.0f);
-//            case EXPOSURE:
-//                return new GPUImageExposureFilter(0.0f);
-//            case HIGHLIGHT_SHADOW:
-//                return new GPUImageHighlightShadowFilter(0.0f, 1.0f);
-//            case MONOCHROME:
-//                return new GPUImageMonochromeFilter(1.0f, new float[]{0.6f, 0.45f, 0.3f, 1.0f});
-//            case OPACITY:
-//                return new GPUImageOpacityFilter(1.0f);
-//            case RGB:
-//                return new GPUImageRGBFilter(1.0f, 1.0f, 1.0f);
-//            case WHITE_BALANCE:
-//                return new GPUImageWhiteBalanceFilter(5000.0f, 0.0f);
-//            case VIGNETTE:
-//                PointF centerPoint = new PointF();
-//                centerPoint.x = 0.5f;
-//                centerPoint.y = 0.5f;
-//                return new GPUImageVignetteFilter(centerPoint, new float[]{0.0f, 0.0f, 0.0f}, 0.3f, 0.75f);
-//            case TONE_CURVE:
-//                GPUImageToneCurveFilter toneCurveFilter = new GPUImageToneCurveFilter();
-//                toneCurveFilter.setFromCurveFileInputStream(
-//                        context.getResources().openRawResource(R.raw.tone_cuver_sample));
-//                return toneCurveFilter;
-//            case BLEND_DIFFERENCE:
-//                return createBlendFilter(context, GPUImageDifferenceBlendFilter.class);
-//            case BLEND_SOURCE_OVER:
-//                return createBlendFilter(context, GPUImageSourceOverBlendFilter.class);
-//            case BLEND_COLOR_BURN:
-//                return createBlendFilter(context, GPUImageColorBurnBlendFilter.class);
-//            case BLEND_COLOR_DODGE:
-//                return createBlendFilter(context, GPUImageColorDodgeBlendFilter.class);
-//            case BLEND_DARKEN:
-//                return createBlendFilter(context, GPUImageDarkenBlendFilter.class);
-//            case BLEND_DISSOLVE:
-//                return createBlendFilter(context, GPUImageDissolveBlendFilter.class);
-//            case BLEND_EXCLUSION:
-//                return createBlendFilter(context, GPUImageExclusionBlendFilter.class);
-//
-//
-//            case BLEND_HARD_LIGHT:
-//                return createBlendFilter(context, GPUImageHardLightBlendFilter.class);
-//            case BLEND_LIGHTEN:
-//                return createBlendFilter(context, GPUImageLightenBlendFilter.class);
-//            case BLEND_ADD:
-//                return createBlendFilter(context, GPUImageAddBlendFilter.class);
-//            case BLEND_DIVIDE:
-//                return createBlendFilter(context, GPUImageDivideBlendFilter.class);
-//            case BLEND_MULTIPLY:
-//                return createBlendFilter(context, GPUImageMultiplyBlendFilter.class);
-//            case BLEND_OVERLAY:
-//                return createBlendFilter(context, GPUImageOverlayBlendFilter.class);
-//            case BLEND_SCREEN:
-//                return createBlendFilter(context, GPUImageScreenBlendFilter.class);
-//            case BLEND_ALPHA:
-//                return createBlendFilter(context, GPUImageAlphaBlendFilter.class);
-//            case BLEND_COLOR:
-//                return createBlendFilter(context, GPUImageColorBlendFilter.class);
-//            case BLEND_HUE:
-//                return createBlendFilter(context, GPUImageHueBlendFilter.class);
-//            case BLEND_SATURATION:
-//                return createBlendFilter(context, GPUImageSaturationBlendFilter.class);
-//            case BLEND_LUMINOSITY:
-//                return createBlendFilter(context, GPUImageLuminosityBlendFilter.class);
-//            case BLEND_LINEAR_BURN:
-//                return createBlendFilter(context, GPUImageLinearBurnBlendFilter.class);
-//            case BLEND_SOFT_LIGHT:
-//                return createBlendFilter(context, GPUImageSoftLightBlendFilter.class);
-//            case BLEND_SUBTRACT:
-//                return createBlendFilter(context, GPUImageSubtractBlendFilter.class);
-//            case BLEND_CHROMA_KEY:
-//                return createBlendFilter(context, GPUImageChromaKeyBlendFilter.class);
-//            case BLEND_NORMAL:
-//                return createBlendFilter(context, GPUImageNormalBlendFilter.class);
-//
-//            case LOOKUP_AMATORKA:
-//                GPUImageLookupFilter amatorka = new GPUImageLookupFilter();
-//                amatorka.setBitmap(BitmapFactory.decodeResource(context.getResources(), R.drawable.amatorka));
-//                return amatorka;
-//            case GAUSSIAN_BLUR:
-//                return new GPUImageGaussianBlurFilter();
-//            case CROSSHATCH:
-//                return new GPUImageCrosshatchFilter();
-//
-//            case BOX_BLUR:
-//                return new GPUImageBoxBlurFilter();
-//            case CGA_COLORSPACE:
-//                return new GPUImageCGAColorspaceFilter();
-//            case DILATION:
-//                return new GPUImageDilationFilter();
-//            case KUWAHARA:
-//                return new GPUImageKuwaharaFilter();
-//            case RGB_DILATION:
-//                return new GPUImageRGBDilationFilter();
-//            case SKETCH:
-//                return new GPUImageSketchFilter();
-//            case TOON:
-//                return new GPUImageToonFilter();
-//            case SMOOTH_TOON:
-//                return new GPUImageSmoothToonFilter();
-//
-//            case BULGE_DISTORTION:
-//                return new GPUImageBulgeDistortionFilter();
-//            case GLASS_SPHERE:
-//                return new GPUImageGlassSphereFilter();
-//            case HAZE:
-//                return new GPUImageHazeFilter();
-//            case LAPLACIAN:
-//                return new GPUImageLaplacianFilter();
-//            case NON_MAXIMUM_SUPPRESSION:
-//                return new GPUImageNonMaximumSuppressionFilter();
-//            case SPHERE_REFRACTION:
-//                return new GPUImageSphereRefractionFilter();
-//            case SWIRL:
-//                return new GPUImageSwirlFilter();
-//            case WEAK_PIXEL_INCLUSION:
-//                return new GPUImageWeakPixelInclusionFilter();
-//            case FALSE_COLOR:
-//                return new GPUImageFalseColorFilter();
-//            case COLOR_BALANCE:
-//                return new GPUImageColorBalanceFilter();
-//            case LEVELS_FILTER_MIN:
-//                GPUImageLevelsFilter levelsFilter = new GPUImageLevelsFilter();
-//                levelsFilter.setMin(0.0f, 3.0f, 1.0f);
-//                return levelsFilter;
-//
-//            default:
-//                throw new IllegalStateException("No filter of that type!");
-//        }
-//
-//    }
+    private static String getFilterNameFromFileName(String filterName) {
+        return Strings.splitReplaceAndJoin("_", " ", FileUtils.removeExtension(filterName),
+                new Strings.Replacer() {
+                    @Override
+                    public String getReplacement(String source) {
+                        return Strings.capitalize(source);
+                    }
+                });
+    }
 
     private static GPUImageFilter createBlendFilter(Context context, Class<? extends GPUImageTwoInputFilter> filterClass) {
         try {
